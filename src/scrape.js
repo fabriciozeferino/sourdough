7153d81ca3bf8347fbde5106a4896acc7c0d6911
fabriciozeferino <fabriciozeferino@gmail.com>
2021-03-28 17:27:58 +0100

    scrape data

--- src/scrape.js
+++ src/scrape.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer');
 
-const scrapePromotions = async function() {
+const scrapePromotions = async function () {
 	const URL = 'https://www2.hm.com/en_gb/sale/kids/babygirls-size4-24m.html';
 
-	const browser = await puppeteer.launch({headless: false});
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-gpu', '--disable-setuid-sandbox'],
+	});
 
 	const page = await browser.newPage();
 
-	await page.goto(URL);
+	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
+
+	await page.goto(URL,  {waitUntil: 'networkidle2'});
+	// await page.screenshot({path: 'buddy-screenshot.png'});
 
 	await page.waitForSelector('#onetrust-accept-btn-handler');
 	await page.click('#onetrust-accept-btn-handler');
@@ -40,7 +46,7 @@ const scrapePromotions = async function() {
 		}
 
 		products.forEach(product => {
-			return results.push(
+				return results.push(
 					{
 						id: getId(product.querySelector('article')),
 						title: textContent(product.querySelector('a.link')),
