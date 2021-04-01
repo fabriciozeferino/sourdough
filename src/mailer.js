12701744425efde42b59cd218ac790155f33f912
fabriciozeferino <fabriciozeferino@gmail.com>
2021-04-01 22:42:29 +0100

    config gmail

--- src/mailer.js
+++ src/mailer.js
@@ -1,39 +1,26 @@
 'use strict';
 require('dotenv').config()
-
 const nodemailer = require('nodemailer');
 
 const main = async (emailBody) => {
 	try {
-
-		console.log({
-			host: process.env.MAIL_HOST,
-			port: process.env.MAIL_PORT,
-			auth: {
-				user: process.env.MAIL_USER,
-				pass: process.env.MAIL_PASS
-			}
-		})
-
-		//Create a SMTP transporter object
 		let transporter = nodemailer.createTransport({
-			host: process.env.MAIL_HOST,
-			port: process.env.MAIL_PORT,
+			service: 'gmail',
 			auth: {
-				user: process.env.MAIL_USER,
-				pass: process.env.MAIL_PASS
+				user: process.env.EMAIL,
+				pass: process.env.PASSWORD
 			}
 		});
 
 		// Message object
 		let message = {
-			from: 'Fabricio <jacquelyn.lindgren81@ethereal.email>',
+			from: 'Fabricio <fabriciozeferino@gmail.com>',
 
 			// Comma separated list of recipients
 			to: 'Viviana <fabriciozeferino84@gmail.com>',
 
 			// Subject of the message
-			subject: 'Nodemailer is unicode friendly ✔',
+			subject: 'Latest sales added to H&M ✔',
 
 			// HTML body
 			html: emailBody
@@ -49,18 +36,22 @@ const main = async (emailBody) => {
 	}
 }
 
-const newEmail = function (items){
+const newEmail = function (items) {
 	let emailBody = `<div style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; -webkit-text-size-adjust: none; background-color: #ffffff; color: #718096; height: 100%; line-height: 1.4; margin: 0; padding: 0; width: 300px !important;">`;
 
-	items.forEach(item => {
 
-		emailBody += `<div style="width: 300px">
-	<div style="padding-bottom: 16px">
-		<img src="${item.imageUrl}" alt="" width="300"/>
-		<p>${item.title}</p>
-		<p>${item.price}</p>
-	</div>
-</div>`
+	items.forEach(item => {
+		emailBody += `
+			<div style="width: 300px">
+				<div style="padding-bottom: 16px;">
+					<a href="https://www2.hm.com/en_gb/productpage.${item.id}.html" target="_blank">
+						<img src="${item.imageUrl}" alt="" width="300"/>
+						<p>${item.title}</p>
+						<p>${item.price}</p>
+					</a>
+				</div>
+			</div>
+		`
 	})
 
 	emailBody += `</div>`;
